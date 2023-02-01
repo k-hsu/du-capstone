a7bc243602343fa0b0dfcbe08277ec173f07641c
k-hsu <khsuzmail@gmail.com>
2023-01-31 19:49:22 -0600

    removed unit tests for toast components

--- frontend/__tests__/providers/toast-context.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ToastContext from "../../providers/contexts/ToastContext";
-
-describe("Toast context", () => {
-  it("should render toast context that is not null", () => {
-    expect(<ToastContext />).not.toBeNull();
-  });
-});
--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,8 +3,15 @@ module.exports = {
   verbose: false,
   testMatch: ["**/__tests__/**/*.test.[jt]s?(x)"],
   moduleNameMapper: {
-    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
   },
+  coveragePathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/providers/",
+    "<rootDir>/hooks/",
+    "<rootDir>/components/Toast/",
+    "<rootDir>/components/ToastManager/",
+  ],
   coverageThreshold: {
     global: {
       branches: 0,
@@ -13,5 +20,5 @@ module.exports = {
       statements: 0,
     },
   },
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.js']
+  setupFilesAfterEnv: ["<rootDir>/jest-setup.js"],
 };
